6a21d9030b100171e720012e9593f49f1ebf0019
Niru Bhandari <nirubhandari45@gmail.com>
2025-04-17 22:19:06 +0545

    feat : routes added

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -1,3 +1,15 @@
 import { Hono } from "hono";
-const app = new Hono();
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+import { LoginSchema } from "../schemas";
+
+const app = new Hono().post(
+  "/login",
+  zValidator("json", LoginSchema),
+  async (c) => {
+    const { email, password } = c.req.valid("json");
+    console.log({ email, password });
+    return c.json({ success: "ok" });
+  }
+);
 export default app;
