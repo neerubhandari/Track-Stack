fed89be9ada7403a6214c1733c10696d6e9894c9
Niru Bhandari <nirubhandari45@gmail.com>
2025-04-21 23:00:59 +0545

    feat : logout link added

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -4,27 +4,39 @@ import { zValidator } from "@hono/zod-validator";
 import { LoginSchema, SignUpSchema } from "../schemas";
 import { createAdminClient } from "@/lib/appwrite";
 import { ID } from "node-appwrite";
-import { setCookie } from "hono/cookie";
+import { deleteCookie, setCookie } from "hono/cookie";
 import { AUTH_COOKIE } from "../constants";
 
 const app = new Hono()
   .post("/login", zValidator("json", LoginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
-    console.log({ email, password });
-    return c.json({ success: "ok" });
-  })
-  .post("/register", zValidator("json", SignUpSchema), async (c) => {
-    const { email, password, name } = c.req.valid("json");
     const { account } = await createAdminClient();
-    const user = await account.create(ID.unique(), email, password, name);
     const session = await account.createEmailPasswordSession(email, password);
     setCookie(c, AUTH_COOKIE, session.secret, {
       path: "/",
       httpOnly: true,
       secure: true,
       sameSite: "strict",
-      maxAge: 60 * 60 * 24 * 30, // 30 days
+      maxAge: 60 * 60 * 24 * 30,
     });
-    return c.json({ data: user });
+    return c.json({ success: true });
+  })
+  .post("/register", zValidator("json", SignUpSchema), async (c) => {
+    const { email, password, name } = c.req.valid("json");
+    const { account } = await createAdminClient();
+    await account.create(ID.unique(), email, password, name);
+    const session = await account.createEmailPasswordSession(email, password);
+    setCookie(c, AUTH_COOKIE, session.secret, {
+      path: "/",
+      httpOnly: true,
+      secure: true,
+      sameSite: "strict",
+      maxAge: 60 * 60 * 24 * 30,
+    });
+    return c.json({ success: true });
+  })
+  .post("/logout", async (c) => {
+    deleteCookie(c, AUTH_COOKIE);
+    return c.json({ success: true });
   });
 export default app;
