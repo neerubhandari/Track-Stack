553b4f260f94a9d7e88cae6363e1b1f2cea4b306
Niru Bhandari <nirubhandari45@gmail.com>
2025-04-23 22:36:55 +0545

    feat : invalidating the current query after the action

--- src/features/auth/api/use-register.ts
+++ src/features/auth/api/use-register.ts
@@ -1,6 +1,7 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 import { client } from "@/lib/rpc";
+import { useRouter } from "next/navigation";
 
 type ResponseType = InferResponseType<
   (typeof client.api.auth.register)["$post"]
@@ -8,11 +9,19 @@ type ResponseType = InferResponseType<
 type RequestType = InferRequestType<
   (typeof client.api.auth.register)["$post"]
 >["json"];
+
 export const useRegister = () => {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
   const mutation = useMutation<ResponseType, Error, RequestType>({
     mutationFn: async (json) => {
       const res = await client.api.auth.register["$post"]({ json });
       return await res.json();
+    },
+    onSuccess: () => {
+      router.refresh();
+      queryClient.invalidateQueries({ queryKey: ["current"] });
     },
   });
   return mutation;
