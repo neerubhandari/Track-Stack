1eb0c467ab5d3796b4b97b6f0ae2b5e120499902
Niru Bhandari <nirubhandari45@gmail.com>
2025-04-23 21:21:45 +0545

    feat : logout button added in the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,24 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Home() {
   const router = useRouter();
   const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
   useEffect(() => {
     if (!data && !isLoading) {
       router.push("/sign-in");
     }
   }, [data]);
-  return <div>Only visible to authorized users</div>;
+  return (
+    <div>
+      Only visible to authorized users
+      <Button onClick={() => mutate()}>Logout</Button>
+    </div>
+  );
 }
