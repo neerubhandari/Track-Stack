957dda7801d21545f922cde7ba5d49c5383fe23b
Niru Bhandari <nirubhandari45@gmail.com>
2025-04-24 19:57:08 +0545

    feat : component switcher added

---
+++ src/components/workspace-switcher.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
+import { RiAddCircleFill } from "react-icons/ri";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+
+export type WorkspaceDocument = {
+  [x: string]: any;
+  $id: string;
+  $collectionId: string;
+  $databaseId: string;
+  $createdAt: string;
+  $updatedAt: string;
+  $permissions: string[];
+  name: string;
+};
+
+export const WorkspaceSwitcher = () => {
+  const { data: workspaces } = useGetWorkspaces();
+
+  return (
+    <div className="flex flex-col gap-y-2">
+      <div className="flex items-center justify-between">
+        <p className="text-xs uppercase text-neutral-500">Workspaces</p>
+        <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer  hover:opacity-75 transition" />
+      </div>
+      <Select>
+        <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
+          <SelectValue placeholder="No workspace selected" />
+        </SelectTrigger>
+        <SelectContent>
+          {workspaces?.documents?.map((workspace: WorkspaceDocument) => (
+            <SelectItem key={workspace.$id} value={workspace.$id}>
+              <div className="flex justify-start items-center gap-3 font-medium ">
+                <WorkspaceAvatar
+                  image={workspace.imageUrl}
+                  name={workspace.name}
+                />
+                <span className="truncate">{workspace.name}</span>
+              </div>
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
+  );
+};
