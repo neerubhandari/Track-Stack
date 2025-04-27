75317ab05075a6a47190162e9684333b937860f9
Niru Bhandari <nirubhandari45@gmail.com>
2025-04-27 23:18:37 +0545

    feat : invite to workspace feature added

---
+++ src/app/(standalone)/workspaces/[workspaceId]/join/[inviteCode]/page.tsx
@@ -0,0 +1,31 @@
+import { getCurrent } from "@/features/auth/queries";
+import { JoinWorkspaceForm } from "@/features/workspaces/components/join-workspace-form";
+import { getWorkspaceInfo } from "@/features/workspaces/queries";
+import { redirect } from "next/navigation";
+import React from "react";
+
+interface WorkspaceIdJoinProps {
+  params: {
+    workspaceId: string;
+  };
+}
+
+const workspaceIdJoinPage = async ({ params }: WorkspaceIdJoinProps) => {
+  const user = await getCurrent();
+
+  if (!user) redirect("/sign-in");
+
+  const initialValues = await getWorkspaceInfo({
+    workspaceId: params.workspaceId,
+  });
+
+  if (!initialValues) redirect("/");
+
+  return (
+    <div className="w-full lg:max-w-xl">
+      <JoinWorkspaceForm initialValues={initialValues} />
+    </div>
+  );
+};
+
+export default workspaceIdJoinPage;
